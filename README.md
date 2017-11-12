56e9594e828841bd4465c6d9b12e77588a48be89
bio-tarik <bio.tarik@gmail.com>
2017-11-12 09:17:22 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodeWebPack
 A very basic node + webpack application template
+
+>npm install
+>npm run dev
+>http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./js/app.js",
+  module: {
+    loaders: [
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "app.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
